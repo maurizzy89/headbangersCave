59c5ea19192c469af05f524c67c80a6e42da8bad
maurizzy89 <maurizzypugliezzy@hotmail.com>
2022-08-09 23:29:57 +0200

    intento4

--- src/main/java/mauriNetwork/mauriPrueba/controladores/PortalControlador.java
+++ src/main/java/mauriNetwork/mauriPrueba/controladores/PortalControlador.java
@@ -32,6 +32,11 @@ public class PortalControlador {
     @Autowired
     private UsuarioServicio usuarioServicio;
 
+    @GetMapping("/favicon.ico")
+    @ResponseBody
+    public void returnNoFavicon() {
+    }
+    
     @GetMapping("/")
     public String index() {
         return "index.html";
